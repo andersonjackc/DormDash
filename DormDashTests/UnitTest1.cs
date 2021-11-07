358b3c2be6234c139115a5617158b36306349329
Brett <bkearney1@ycp.edu>
2021-11-07 00:45:59 -0400

    add sparts admin page

--- DormDashTests/UnitTest1.cs
+++ DormDashTests/UnitTest1.cs
@@ -15,8 +15,7 @@ namespace DormDashTests
             System.Collections.Generic.List<MenuItem> menu_items = DatabaseOperations.selectMenuItems();
 
             Order order = new Order(67, 903242133, DateTime.Now, 100, new Destination(building.Kinsley, 000), menu_items,false);
-            DatabaseOperations.insertUser(new User(903242133, UserType.Employee, 20.00, 20.00, "test@ycp.edu", "test123"));
-            DatabaseOperations.updateUser(new User(903242133, UserType.Employee, 69.00, 21.00, "gggggggggggggg1@ycp.edu"));
+            DatabaseOperations.updateUser(new User(900, UserType.Employee, 69.00, 21.00, "gggggggggggggg1@ycp.edu"));
             DatabaseOperations.insertOrder(order);
         }
 
