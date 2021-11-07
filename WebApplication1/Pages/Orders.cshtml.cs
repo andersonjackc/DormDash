2a4f309ebe8f587c28705b48a8dc0a08750acec6
DanPalm5 <dpalmieri@ycp.edu>
2021-11-06 20:19:05 -0400

    Work on order pages

--- WebApplication1/Pages/Orders.cshtml.cs
+++ WebApplication1/Pages/Orders.cshtml.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1;
+using Microsoft.AspNetCore.Http;
 
 namespace DormDash.Pages
 {
     public class OrdersModel : PageModel
     {
+        UserType userType;
         public void OnGet()
         {
+
+            if(userType == UserType.Customer)
+            {
+                User loggedInCustomer = Utils.GetComplexObject<User>(HttpContext.Session, "user");
+
+                List<Order> orderList = DatabaseOperations.selectOrdersByUserID(loggedInCustomer.id);
+                HttpContext.Session.SetComplexObject<List<Order>>("orderList", orderList);
+            }
+            else if(userType == UserType.Employee)
+            {
+                List<Order> ordersReadyToPick;
+            }
+            else
+            {
+                // Sparts admin
+            }
+
         }
     }
 }
